2a55ec9f520fb0aaf13e47ba9a93172291df34d5
Alex Tomas <atomasllimos@gmail.com>
2022-10-21 09:28:52 +0200

    Fix some bugs

--- src/pages/Home.ts
+++ src/pages/Home.ts
@@ -8,8 +8,6 @@ const $app = $('#app')
 const Home = async (): Promise<Error | void> => {
     if ($app === null) throw new Error('Ups!')
 
-    console.log('b')
-
     $app.innerHTML += `
         <div class="container">
             <h1>COST OF LIVING IN <span class="living-in">...</span></h1>
@@ -18,46 +16,46 @@ const Home = async (): Promise<Error | void> => {
                     <input type="text" class="search-engine" placeholder="New York" autocomplete="off">
                 </label>
             </form>
+
+            <div id="prices" class="container" ></div>
                 
         </div>
     `
-
-    const $form = $('form')
     const $input = $('input') as HTMLInputElement | null
 
-    $form?.addEventListener('submit', async (evt: Event) => {
-        console.log('a')
+    document.addEventListener('submit', async (evt: Event) => {
+
         evt.preventDefault()
+        const $prices = $('#prices')
+        if ($prices === null) throw new Error('Oups')
+        $prices.innerHTML = ''
+
         if ($input === null || $input.value === null) throw new Error('Oops, something wrong happened')
         const { value: cityNameFromUser } = $input
         const { cities } = localStorage.cities()
 
         if (!cities) throw new Error('An error has occured. Please try again later')
 
-        console.log(cities.find(({ city_name }) => city_name === cityNameFromUser) as City)
-
         const foundCity = cities.find(({ city_name }) => city_name === cityNameFromUser)
 
         if (foundCity === undefined) {
-            $app.innerHTML += '<span class="error">Sorry, The city you are looking for has not been found</span>'
+            $prices.innerHTML = '<h2 class="error">Sorry, The city you are looking for has not been found</h2>'
+            $input.value = ''
             throw new Error('City are looking for for has not been found')
-
         }
 
         const { city_name, country_name } = foundCity
 
-        if (!city_name && !country_name) $app.innerHTML += `<h3 class="not-found">${cityNameFromUser} not found</h3>`
+        const prices = await getPrices(city_name, country_name)
 
-        else {
-            const prices = await getPrices(city_name, country_name)
+        if (!prices) throw new Error('An error has occured. Please try again later')
 
-            if (!prices) throw new Error('An error has occured. Please try again later')
-
-            const $livingIn = $('.living-in')
-            if (!$livingIn) throw new Error('ups!')
-            $livingIn.innerHTML = city_name.toUpperCase()
-            $app.innerHTML += `
+        const $livingIn = $('.living-in')
+        if (!$livingIn) throw new Error('ups!')
+        $livingIn.innerHTML = city_name.toUpperCase()
+        $prices.innerHTML = `
                 <div class="container">
+                <h2>${city_name}</h2>
                  <ul>
                     ${removeComas(prices.map(({ item_name, avg }) => `<li><span>${item_name}</span>  <span>${avg}$</span></li>`).toString())}
                  </ul>
@@ -65,9 +63,9 @@ const Home = async (): Promise<Error | void> => {
                 
                 </div>
             `
-        }
-    })
+        $input.value = ''
 
+    })
 }
 
 
